49001f0c742877fe3dea5039cc2a630bef3cb1a0
othenone <olavnon@gmail.com>
2023-04-26 11:49:27 +0200

    Created loading icon

--- src/lib/Functions/MapLayers.ts
+++ src/lib/Functions/MapLayers.ts
@@ -3,12 +3,14 @@
 import L from "leaflet";
 import { convertF } from "./ConvertUnit";
 import { chartF } from "./Chart";
+import type CSS from 'csstype'
 
 // Imports from Stores.svelte:
 import type { Point, Ridge, Crd, Dataset } from "../Stores";
 import { chartTypes } from "../Stores";
 
-var markerIcon = L.icon({
+// The marker icon:
+let markerIcon = L.icon({
   iconUrl: 'marker.svg',
   shadowUrl: 'marker-shadow.svg',
 
@@ -17,6 +19,39 @@ var markerIcon = L.icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 })
+
+// Create the loading HTML element:
+let loadingIconHTML = document.createElement("div");
+
+// Create the CSS of the loading icon:
+let loadingIconStyle: CSS.Properties = {
+  translate: "-50% -50%",
+  border: "3px solid #f3f3f3",
+  borderRadius: "50%",
+  borderTop: "3px solid #3498db",
+  width: "20px",
+  height: "20px",
+}
+
+// Assign the made properties to the loading HTML element:
+Object.assign(loadingIconHTML.style, loadingIconStyle);
+
+// Make the loading HTML element spin:
+loadingIconHTML.animate([
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+  duration: 2000,
+  iterations: Infinity
+})
+
+// Create the Leaflet loading icon:
+let loadingIcon = L.divIcon({
+  iconSize: [0, 0],
+  className: "Something to remove the white background",
+  html: loadingIconHTML,
+})
+
 
 export function createPolyline(crds?: Crd[]) {
 
